a2f527109e865dfdd887447f7d51afdeee630127
xull <284904817@qq.com>
2018-10-29 23:03:50 +0800

    长连接 框架搭建

---
+++ machine/app/src/main/java/com/dryer/xull/service/IBackService.java
@@ -0,0 +1,9 @@
+package com.dryer.xull.service;
+
+interface IBackService {
+    /**
+     * Demonstrates some basic types that you can use as parameters
+     * and return values in AIDL.
+     */
+    boolean sendMessage(String message);
+}
